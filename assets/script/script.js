f6d5af66250450e9e3b9738ae41eab3f816c293b
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-10-10 17:22:00 +0200

    style nav and btn component

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,35 +4,36 @@ const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const nav = document.getElementById("nav");
 
-let openMenu;
-let closeMenu;
+
+let windowWidth = window.innerWidth;
+
+let openMenu = function () {
+    mobileMenu.style.height = "calc(100vh - 80px)";
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "flex";
+    nav.style.background = "#1c1719";
+    nav.style.color = "white";
+    nav.style.transition = "300ms ease-in-out";
+}
+
+/* Close */
+let closeMenu = function () {
+    mobileMenu.style.height = "0";
+    menuBtn.style.display = "flex";
+    closeBtn.style.display = "none";
+    nav.style.background = "#f0f5f5";
+    nav.style.color = "black";
+    nav.style.transition = "300ms ease-in-out";
+}
 
 window.addEventListener('resize', function () {
     console.log("hey");
-    let windowWidth = window.innerWidth;
-    if (windowWidth < 664) {
-        /* Open */
-        openMenu = function () {
-            mobileMenu.style.height = "calc(100% - 80px)";
-            document.getElementById("menu-content").style.height = "100%";
-            menuBtn.style.display = "none";
-            closeBtn.style.display = "flex";
-            nav.style.background = "#1c1719"
-            nav.style.color = "white";
-            nav.style.transition = "300ms ease-in-out";
-        }
-
-        /* Close */
-        closeMenu = function () {
-            mobileMenu.style.height = "0";
-            document.getElementById("menu-content").style.height = "0";
-            menuBtn.style.display = "flex";
-            closeBtn.style.display = "none";
-            nav.style.background = "#f0f5f5"
-            nav.style.color = "black";
-            nav.style.transition = "300ms ease-in-out";
-        }
-    } else {
+    if (windowWidth > 663) {
         menuBtn.style.removeProperty("display");
+        mobileMenu.style.removeProperty("height");
+        closeBtn.style.removeProperty("display");
+        nav.style.removeProperty("background");
+        nav.style.removeProperty("color");
+        nav.style.removeProperty("transition");
     }
-})
+});
